0df758b226b6bb95c650433d6e537209c7fb0022
ayelen.rey <ayelen.rey@bbva.com>
2017-10-16 15:32:53 -0300

    agrego tratamiento para queryparameters ausente, se retorna 400.

--- server/boot/coords.js
+++ server/boot/coords.js
@@ -10,29 +10,37 @@ var googleMapsClient = require('@google/maps').createClient({
 
 module.exports = function(server) {
 
-
-
   var router = server.loopback.Router();
   router.get('/cords', (req,res) => {
     console.log('entre');
     		  /**  GOOGLE MAPS API Geocode  **/		
           var address = req.query['address'];		
-          googleMapsClient.geocode({
-          address: address
-            }, (err, response) => {
-            console.log('mapsClient.geocode');
-          if (!err) {
-            var rtaMap = {};
-            rtaMap.nombreLargo  = response.json.results[0].address_components[0].long_name;
-            rtaMap.nombreFormateado = JSON.stringify(response.json.results[0].formatted_address);
-            rtaMap.lat = JSON.stringify(response.json.results[0].geometry.location.lat);
-            rtaMap.lng = JSON.stringify(response.json.results[0].geometry.location.lng);
-  
-            //console.log('api rtaMap: ' , JSON.stringify(rtaMap));
-            res.end(JSON.stringify(rtaMap));
+          if ("address" in req.query) {
+            
+            googleMapsClient.geocode({
+            address: address
+              }, (err, response) => {
+              console.log('mapsClient.geocode');
+            if (!err) {
+              var rtaMap = {};
+              rtaMap.nombreLargo  = response.json.results[0].address_components[0].long_name;
+              rtaMap.nombreFormateado = JSON.stringify(response.json.results[0].formatted_address);
+              rtaMap.lat = JSON.stringify(response.json.results[0].geometry.location.lat);
+              rtaMap.lng = JSON.stringify(response.json.results[0].geometry.location.lng);
+    
+              //console.log('api rtaMap: ' , JSON.stringify(rtaMap));
+              res.end(JSON.stringify(rtaMap));
+            }
+            });
           }
-          });
-    } );
+          else {
+            
+          console.log('entre a blancos');
+          res.writeHead(404);
+          res.end('Content not found!');
+          }
+        
+      } );
   console.log('corriendo cords!');
   server.use(router);
 };
