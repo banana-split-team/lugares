558ffecba56a05bb773d6b7e0f0b49b5fe2f0786
Diego Alfonso Oderigo <diegoalfonso.oderigo@bbva.com>
2017-10-20 14:13:09 -0300

    Mas correcciones del linter, mayormente identacion

--- test/cords.js
+++ test/cords.js
@@ -31,48 +31,49 @@ describe('Escenario 2', function() {
       function() {
         it('Debería devolver 200 y, debería estar insertado en la BD \
             el lugar guardado', function() {
-        chai.request(server)
-          .post('/api/lugares')
-          .send({
-            'lugar': 'Torre BBVA',
-            'coord': {
-              'lat': -34.5980009,
-              'lng': -58.3702542
-            }
-          }).end(function(err, res) {
-            res.should.have.status(200);
-            res.should.be.json;
-            res.body.should.be.a('object');
-            res.body.should.have.property('SUCCESS');
-            res.body.SUCCESS.should.be.a('object');
-          });
+          chai.request(server)
+            .post('/api/lugares')
+            .send({
+              'lugar': 'Torre BBVA',
+              'coord': {
+                'lat': -34.5980009,
+                'lng': -58.3702542,
+              },
+            }).end(function(err, res) {
+              res.should.have.status(200);
+              res.should.be.json();
+              res.body.should.be.a('object');
+              res.body.should.have.property('SUCCESS');
+              res.body.SUCCESS.should.be.a('object');
+            });
         });
       });
 });
 
 describe('Escenario 3', function() {
-  describe('Usuario ingresa ya consulto "Ahaskdjasdqieuhhadlakjchhaskdjadsdasd" \
-      y presiona consultar.', function() {
-    it('No debería devolver resultados', function() {
-      chai.request(server)
-        .get('/cords?address=Ahaskdjasdqieuhhadlakjchhaskdjadsdasd')
-        .end(function(err, res) {
-          res.should.have.status(200);
-        });
+  describe('Usuario ingresa ya consulto \
+      "Ahaskdjasdqieuhhadlakjchhaskdjadsdasd" y presiona consultar.',
+      function() {
+        it('No debería devolver resultados', function() {
+          chai.request(server)
+            .get('/cords?address=Ahaskdjasdqieuhhadlakjchhaskdjadsdasd')
+            .end(function(err, res) {
+              res.should.have.status(200);
+            });
+          });
       });
-  });
 });
 
 describe('Escenario 4', function() {
     describe('Consultar sus direcciones guardadas como opcion en el menu.',
         function() {
-      it('Deberíamos ver todas las direcciones guardadas (Get a /api/lugares \
-          deberia devolver 200)', function() {
-        chai.request(server)
-          .get('/api/lugares')
-          .end(function(err, res) {
-            res.should.have.status(200);
+          it('Deberíamos ver todas las direcciones guardadas (Get a /api/lugares \
+              deberia devolver 200)', function() {
+            chai.request(server)
+              .get('/api/lugares')
+              .end(function(err, res) {
+                res.should.have.status(200);
+            });
+          });
         });
-      });
-    });
 });
