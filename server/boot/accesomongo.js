5939c73ec8a6c62f12d104d8fec536960750af49
Diego Alfonso Oderigo <diegoalfonso.oderigo@bbva.com>
2017-10-23 11:46:13 -0300

    Corrijo errores de linter

--- server/boot/accesomongo.js
+++ server/boot/accesomongo.js
@@ -1,4 +1,5 @@
 'use strict';
+
 const http = require('http');
 const url = require('url');
 const qs = require('querystring');
@@ -13,20 +14,18 @@ module.exports = function(server) {
   router.get('/mongotest', (req, res) => {
     console.log('mongotest entre');
     var MongoClient = require('mongodb').MongoClient;
-    var url = "mongodb://ds113795.mlab.com:13795/bananasplit";
-    
+    var url = 'mongodb://ds113795.mlab.com:13795/bananasplit';
     MongoClient.connect(url, function(err, db) {
       if (err) throw err;
-      db.authenticate("bananasplit", "bsbbva2017", function(err, result) {
+      db.authenticate('bananasplit', 'bsbbva2017', function(err, result) {
         if (err) throw err;
-        db.collection("lugares").findOne({}, function(err, result) {
-        if (err) throw err;
+        db.collection('lugares').findOne({}, function(err, result) {
+          if (err) throw err;
           console.log(result);
           db.close();
         });
       });
     });
-
   });
   console.log('corriendo mongotest!');
   console.log(configDB.db.password);
